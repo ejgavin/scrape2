876a3b15d3288bcd3270c302da40db49aa3a071a
ejgavin <144240573+ejgavin@users.noreply.github.com>
2025-05-18 23:23:11 -0400

    Update server.js

--- api/server.js
+++ api/server.js
@@ -3,16 +3,6 @@ import axios from 'axios';
 export default async function handler(req, res) {
   const targetUrl = req.query.url;
 
-  // Set CORS headers
-  res.setHeader('Access-Control-Allow-Origin', 'https://ejgavin.github.io');
-  res.setHeader('Access-Control-Allow-Methods', 'GET, OPTIONS');
-  res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
-
-  // Handle CORS preflight
-  if (req.method === 'OPTIONS') {
-    return res.status(200).end();
-  }
-
   if (!targetUrl) {
     return res.status(400).send('Missing ?url= query parameter');
   }
